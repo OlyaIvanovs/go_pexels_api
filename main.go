0807a6a59170e1bc8b23c8568d03aacb7b08dbdb
olya <olya@ivanovs.info>
2021-10-01 23:03:56 +1300

    Add SearchPhotos method.

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 
 	"github.com/kelseyhightower/envconfig"
 )
@@ -19,15 +22,64 @@ var opt struct {
 
 type Context struct {
 	Token          string
-	HTTPClient     http.Client
+	HTTPCl         http.Client
 	RemainingTimes int32
 }
 
+func (c *Context) SearchPhotos(query string, perPage, page int) (*SearchResult, error) {
+	url := fmt.Sprintf(PhotoApi+"/search?query=%s&per_page=%d&page=%d", query, perPage, page)
+	resp, err := c.requestDoWithAuth("GET", url)
+	defer resp.Body.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var res SearchResult
+	err = json.Unmarshal(data, &res)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &res, nil
+}
+
+func (c *Context) requestDoWithAuth(method, url string) (*http.Response, error) {
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", c.Token)
+	res, err := c.HTTPCl.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	times, err := strconv.Atoi(res.Header.Get("X-Ratelimit-Remaining"))
+	if err != nil {
+		return nil, err
+	}
+
+	c.RemainingTimes = int32(times)
+
+	return res, nil
+
+}
+
 func NewContext(token string) *Context {
-	c := http.Client()
+	c := http.Client{}
 	return &Context{
-		Token:      token,
-		HTTPClient: c,
+		Token:  token,
+		HTTPCl: c,
 	}
 }
 
@@ -37,8 +89,18 @@ func main() {
 		fmt.Printf("Failed to parse command line arguments: %s", err.Error())
 	}
 
-	ctx := Context{
-		Token: opt.Token,
+	ctx := NewContext(opt.Token)
+
+	result, err := ctx.SearchPhotos("waves", 15, 2)
+
+	if err != nil {
+		fmt.Errorf("Search errpr:%v", err)
 	}
+
+	if result.Page == 0 {
+		fmt.Errorf("Search result wrong")
+	}
+
+	fmt.Printf(result)
 
 }
